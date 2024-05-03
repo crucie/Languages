886f2e89c9c9de9ea7892d06085af6bb4e67067d
kosh <amaymishra100@gmail.com>
2024-05-04 04:00:38 +0530

    part 01 of js

--- javaScript/txt.js
+++ javaScript/txt.js
@@ -1,7 +1,101 @@
-console.log("hitesh");
+// // console.log("hitesh");
+// // console.log(true);
+// // console.log(2!=1);
 
-const accountId = 144;
-let accountEmail = "hitesh@google.com";
-var accountPassword = "12345";
+// // console.log(null >=0);
+// // console.log(null>0);
+// // console.log(null<0);
+// // console.log(null==0);
 
-accountCity = "jaipur"
+// // +++++++++++++++++++++++++++++++++++++++++++
+
+
+// // stack(PRimitive), heap(Non-Primitive)
+
+// let myYoutubeName= "gotohell"
+
+// let uswer= {
+//     email : "am@gmail.com", 
+//     pass : "lmao"
+// }
+
+// let userTwo= uswer
+
+// userTwo.email = "hitesh@google.com"
+
+// // string         
+
+// // ++++++++++++++++++++++++++
+
+// const name = "hitesh"
+
+// const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+// console.log( `hello my name is ${name}` )
+
+
+// const gameName = new String('hiteshhc')
+
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.indexOf('t'));
+// const newString = gameName.substring(0,5);
+// console.log(newString);
+
+
+// // +++++++++++++++++++++number++++++++++++++
+
+// const score = 400;
+// console.log(score)
+
+// const balance = new Number(100);
+
+// console.log(balance);
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+ 
+// // +++++++++++++++++++MATHS +++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(6.3));
+// console.log(Math.floor(6.7));
+
+// +++++++++++Dates+++++++++++++++
+
+let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23);
+
+let myCreatedDate = new Date("2023-11-14");
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+// console.log(newDate.getHours());
+// console.log(newDate.getMonth());
+
+// `${newDate.getDay()}`
+
+
+
+
+
+
