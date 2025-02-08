aaa316c4079523b1013b2c8cf2a0f74d9ca86827
Crucie <amaymishra100@gmail.com>
2025-02-08 22:57:00 +0530

    update

---
+++ javaScript/conversionOperator.js
@@ -0,0 +1,45 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+let newp = null
+
+console.log(typeof(newp));
+
+let valN= Number(newp);
+
+console.log(typeof(newp));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "amay";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// console.log(2+2);
+// console.log(2-2); 
+// console.log(2*2);
+
+
+// 1=> true; 0=>false 
+// "" => false
+// "amay" => true
+
+
+let str2 = "hitesh"
+let str4 = "lmaoo"
+let str3 = str2+ str4;
+console.log(str3);
+
+
+ 
+
+
